2db38de820f81778f1211974a9d57c3ecf478a6a
Pazhani <pazhanivel11111@gmail.com>
2024-06-05 15:44:09 +0530

    c

---
+++ src/pages/Sidebar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+    <ul className="sidebar-list">
+      <li className="sidebar-item">
+        <a href="#" className="sidebar-link">Home</a>
+      </li>
+      <li className="sidebar-item">
+        <a href="#" className="sidebar-link">About</a>
+      </li>
+      <li className="sidebar-item">
+        <a href="#" className="sidebar-link">Services</a>
+      </li>
+      <li className="sidebar-item">
+        <a href="#" className="sidebar-link">Contact</a>
+      </li>
+    </ul>
+  </div>
+  )
+}
+
+export default Sidebar
